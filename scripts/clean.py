8185840b99ec554f99102e3d90fa89ef4ac3d768
Chris Holdgraf <choldgraf@berkeley.edu>
2018-10-15 15:06:17 -0700

    adding support for multiple sub-chapters

--- scripts/clean.py
+++ scripts/clean.py
@@ -5,9 +5,9 @@ import os.path as op
 
 path_root = op.join(op.dirname(op.abspath(__file__)), '..')
 
-paths = [op.join(path_root, 'images', 'ch'),
+paths = [op.join(path_root, 'images', 'build'),
          op.join(path_root, '_site'),
-         op.join(path_root, '_ch')]
+         op.join(path_root, '_build')]
 for path in paths:
     print('Removing {}...'.format(path))
     sh.rmtree(path, ignore_errors=True)
